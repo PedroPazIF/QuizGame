f1953e16a7e03171af01b22c8b2dd19ec3f10d44
Pedro Paz <98429573+PedroPazIF@users.noreply.github.com>
2023-10-11 19:57:26 -0300

    First commit

---
+++ quiz.py
@@ -0,0 +1,27 @@
+import json
+
+class Question:
+    def __init__(self, pergunta, opcoes):
+        self.pergunta = pergunta
+        self.opcoes = opcoes
+
+    def __repr__(self):
+        return f"<Question> pergunta: {self.pergunta} - opcoes: {self.opcoes}"
+
+class Quiz:
+    def __init__(self, questions):
+        self.questions = questions
+    
+    def start(self):
+      for question in self.questions:
+        print("\n" + question.pergunta)
+        for i, option in enumerate(question.opcoes, start=1):
+                print(f"{i}. {option}")
+
+if __name__ == "__main__":
+    with open('jogos.json', 'r') as f:
+        data = json.load(f)
+
+    questions = [Question(q['pergunta'], q['opcoes']) for q in data['jogos']]
+    quiz = Quiz(questions)
+    quiz.start()
